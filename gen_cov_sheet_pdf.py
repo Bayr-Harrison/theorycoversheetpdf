98bcd1a3e73bdb9811c9fedcec575eff58c8709e
Bayr-Harrison <121176740+Bayr-Harrison@users.noreply.github.com>
2024-11-21 12:47:39 +0300

    Add files via upload

--- gen_cov_sheet_pdf.py
+++ gen_cov_sheet_pdf.py
@@ -8,6 +8,17 @@ from openpyxl import Workbook
 from openpyxl.styles import Alignment, Font, Border, Side, PatternFill
 from openpyxl.utils import get_column_letter
 
+# Set a simple password
+PASSWORD = os.environ["APP_PASSWORD"]
+
+# Create a password input field in Streamlit
+password = st.text_input("Enter Password", type="password")
+if password != PASSWORD:
+    st.warning("Incorrect password")
+    st.stop()
+else:
+    st.success("Access granted!")
+
 # Function to establish a database connection
 def get_database_connection():
     db_connection = pg8000.connect(
